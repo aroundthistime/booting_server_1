ecc53bb1d052ae92d1e05233ac2c491a4c4811c2
aroundthistime <62099715+aroundthistime@users.noreply.github.com>
2021-01-19 00:03:25 +0900

    modified searchUser query to prevent searching myself

--- src/api/User/searchUser/searchUser.js
+++ src/api/User/searchUser/searchUser.js
@@ -1,12 +1,32 @@
 export default {
   Query: {
-    searchUser: async (_, { term }, { prisma }) => prisma.users({
-      where: {
-        OR: [
-          { username_contains: term },
-          { name_contains: term },
-        ],
-      },
-    }),
+    searchUser: async (_, { term }, { request, prisma }) => {
+      if (request.user){
+        return prisma.users({
+          where: {
+            AND : [
+              {
+                OR: [
+                  { username_contains: term },
+                  { name_contains: term },
+                ],
+              },
+              {
+                id_not : request.user.id
+              }
+            ]
+          },
+        })
+      } else {
+        return prisma.users({
+          where : {
+            OR : [
+              { username_contains: term },
+              { name_contains: term },
+            ]
+          }
+        })
+      }
+    }
   },
 };
